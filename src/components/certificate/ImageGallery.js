b688370667dd5365a5c3898da132b60f12ba8af6
rohitagr0310 <rohitagr2610@gmail.com>
2024-03-18 01:02:30 +0530

    added certificate accordion

--- src/components/certificate/ImageGallery.js
+++ src/components/certificate/ImageGallery.js
@@ -9,6 +9,31 @@ const imageContext = require.context(
 
 const ImageGallery = ({ children }) => {
   const imagePaths = imageContext.keys();
+  const group = {}
+  imagePaths.forEach(element => {
+    const internalGroup = element.split('/')[1];
+    const imagePathString = imageContext(element);
+    const originalFilename = imagePathString
+      ? imagePathString.split("/").pop().split(".")[0]
+      : "Unknown";
+    if (Object.keys(group).includes(internalGroup)) {
+      group[internalGroup].push({
+        title: originalFilename,
+        path: imagePathString,
+      })
+    } else {
+      group[internalGroup] = []
+      group[internalGroup].push({
+        title: originalFilename,
+        path: imagePathString,
+      })
+    }
+  });
+
+  //console.log(group, Object.entries(group), '1')
+
+  Object.entries(group).map((key, value) => console.log(key, value))
+
   const imageInfo = imagePaths.map((imagePath) => {
     const imagePathString = imageContext(imagePath);
     const originalFilename = imagePathString
@@ -21,7 +46,7 @@ const ImageGallery = ({ children }) => {
     };
   });
 
-  return <>{children(imageInfo)}</>;
+  return <>{children(group)}</>;
 };
 
 export default ImageGallery;
